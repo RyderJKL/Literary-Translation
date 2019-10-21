a9bf5168a9ba2ad20558d6c88321480ac7410dad
chenrong <chenrong@lvwan.com>
2019-10-21 21:30:17 +0800

    feata: test commit msg

--- scripts/verifyCommitMsg.js
+++ scripts/verifyCommitMsg.js
@@ -6,6 +6,8 @@ const msg = require('fs')
 
 const commitRE = /^(v\d+\.\d+\.\d+(-(alpha|beta|rc.\d+))?$)|((revert: )?(feat|fix|docs|style|refactor|perf|test|workflow|ci|chore|types)(\(.+\))?!?: .{1,50})/;
 
+console.log('test verifyCommitMsg');
+
 if (!commitRE.test(msg)) {
     console.error(
         `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(`invalid commit message format.`)}\n\n` +
