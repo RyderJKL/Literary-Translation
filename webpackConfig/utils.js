768a14a18cc5f6a0f1b09f06bfe485c46f849a14
chenrong@lvwan.com <chenrong@lvwan.com>
2019-10-31 20:46:57 +0800

    feat: 回退 webpackConifg/utils 文件

--- webpackConfig/utils.js
+++ webpackConfig/utils.js
@@ -4,41 +4,44 @@ const config = require('./config');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const packageConfig = require('../package.json');
 
-exports.assetsPath = function(_path) {
+exports.assetsPath = function (_path) {
     const assetsSubDirectory =
-        process.env.NODE_ENV === 'production' ? config.build.assetsSubDirectory : config.dev.assetsSubDirectory;
+        process.env.NODE_ENV === 'production'
+            ? config.build.assetsSubDirectory
+            : config.dev.assetsSubDirectory;
 
     return path.posix.join(assetsSubDirectory, _path);
 };
 
-exports.generatorCssLoader = function(loader, options) {
+exports.cssLoaders = function (options) {
+    options = options || {};
+
+    const styleLoader = {
+        loader: 'style-loader',
+        options: {
+            sourceMap: options.sourceMap
+        }
+    };
+
+    const cssLoader = {
+        loader: 'css-loader',
+        // options: {
+        //     sourceMap: options.sourceMap,
+        //     modules: true,
+        //     importLoaders: 2,
+        //     localIdentName: '[name]-[local]-[hash:base64:5]'
+        // }
+    };
+
+    const postcssLoader = {
+        loader: 'postcss-loader',
+        options: {
+            sourceMap: options.sourceMap
+        }
+    };
+
     // generate loader string to be used with extract text plugin
-    return function generateLoaders(loaderOptions) {
-        loaderOptions = loaderOptions || {};
-
-        const styleLoader = {
-            loader: 'style-loader',
-            options: {
-                sourceMap: options.sourceMap
-            }
-        };
-
-        const cssLoader = {
-            loader: 'css-loader',
-            options: Object.assign({}, {
-                modules: true,
-                importLoaders: 2,
-                localIdentName: '[name]-[local]-[hash:base64:5]'
-            })
-        };
-
-        const postcssLoader = {
-            loader: 'postcss-loader',
-            options: {
-                sourceMap: options.sourceMap
-            }
-        };
-
+    function generateLoaders(loader, loaderOptions) {
         const loaders = [];
 
         // Extract CSS when that option is specified
@@ -46,13 +49,13 @@ exports.generatorCssLoader = function(loader, options) {
         if (options.extract) {
             loaders.push(MiniCssExtractPlugin.loader);
         } else {
-            loaders.push(styleLoader);
+            loaders.push(styleLoader)
         }
 
         loaders.push(cssLoader);
 
         if (options.usePostCSS) {
-            loaders.push(postcssLoader);
+            loaders.push(postcssLoader)
         }
 
         if (loader) {
@@ -61,59 +64,47 @@ exports.generatorCssLoader = function(loader, options) {
                 options: Object.assign({}, loaderOptions, {
                     sourceMap: options.sourceMap
                 })
-            });
+            })
         }
 
-        return loaders;
-    };
-    // return {
-    //     css: generateLoaders,
-    //     postcss: generateLoaders,
-    //     // less: generateLoaders('less'),
-    //     // sass: generateLoaders('sass'),
-    //     scss: generateLoaders,
-    //     // stylus: generateLoaders('stylus'),
-    //     styl: generateLoaders
-    // }
+        return loaders
+    }
+
+    return {
+        css: generateLoaders(),
+        postcss: generateLoaders(),
+        less: generateLoaders('less'),
+        sass: generateLoaders('sass', {
+            indentedSyntax: true,
+            sourceMap: options.sourceMap,
+            modules: true,
+            importLoaders: 2,
+            localIdentName: '[name]-[local]-[hash:base64:5]'
+        }),
+        scss: generateLoaders('sass', {
+            sourceMap: options.sourceMap,
+            modules: true,
+            importLoaders: 2,
+            localIdentName: '[name]-[local]-[hash:base64:5]'
+        }),
+        stylus: generateLoaders('stylus'),
+        styl: generateLoaders('stylus')
+    }
 };
 
-exports.styleLoaders = function(options) {
+exports.styleLoaders = function (options) {
     const output = [];
-    const loaderExtensions = ['css', 'postcss', 'scss'];
-    const loaders = loaderExtensions.map(loader => {
-        return {
-            loaderName: loader,
-            loader: exports.generatorCssLoader(loader === 'scss' ? 'sass' : loader, options)
-        };
-    });
+    const loaders = exports.cssLoaders(options);
 
-    loaders.forEach(loader => {
-        if (loader.loaderName === 'css') {
-            // output.push({
-            //     test: new RegExp('\\.' + loader.loaderName + '$'),
-            //     use: loader.loader(),
-            //     include: /node_modules/,
-            // });
+    for (const extension in loaders) {
+        const loader = loaders[extension];
+        output.push({
+            test: new RegExp('\\.' + extension + '$'),
+            use: loader
+        })
+    }
 
-            output.push({
-                test: new RegExp('\\.' + loader.loaderName + '$'),
-                use: loader.loader({
-                    modules: true,
-                    importLoaders: 2,
-                    localIdentName: '[name]-[local]-[hash:base64:5]'
-                }),
-                exclude: /node_modules/
-            });
-        } else {
-            output.push({
-                test: new RegExp('\\.' + loader.loaderName + '$'),
-                use: loader.loader(),
-                exclude: /node_modules/
-            });
-        }
-    });
-
-    return output;
+    return output
 };
 
 exports.createNotifierCallback = () => {
@@ -130,6 +121,6 @@ exports.createNotifierCallback = () => {
             message: severity + ': ' + error.name,
             subtitle: filename || '',
             icon: path.join(__dirname, 'logo.png')
-        });
-    };
+        })
+    }
 };
