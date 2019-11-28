843b44c919adcc33c22de69480bf463c59a974a0
chenrong@lvwan.com <chenrong@lvwan.com>
2019-11-28 11:51:01 +0800

    feat: basic-layout 删除注释代码，退订 Observable

--- src/layouts/basic-layout/index.tsx
+++ src/layouts/basic-layout/index.tsx
@@ -9,9 +9,6 @@ import $request, { Response } from '@/utils/request';
 import styles from './styles.scss';
 import SideMenuLayout from './components/sidemenu-layout';
 import TopMenuLayout from './components/topmenu-layout';
-// import { useObservable } from 'rxjs-hooks';
-// import { Observable } from 'rxjs';
-import { share } from 'rxjs/operators';
 
 export interface BasicLayoutProps {
     userInfo: UserInfo;
@@ -51,19 +48,15 @@ const BasicLayout: React.FC<BasicLayoutProps> = (props) => {
             return;
         }
 
-        const subscription = $request.get('/user/info', { metas: { mock: true } }).subscribe(
-            (res: Response) => {
-                console.log('haha');
-                // subscription.unsubscribe();
+        const subscription = $request.get('/user/info', { metas: { mock: true } }).subscribe((res: Response) => {
+            if (res.code !== 20000) {
+                router.history.replace(`${rootPath}${loginPath}`);
+            }
 
-                if (res.code !== 20000) {
-                    router.history.replace(`${rootPath}${loginPath}`);
-                }
+            saveUserInfo(res.data.userInfo);
+        });
 
-                saveUserInfo(res.data.userInfo);
-            },
-            (error) => console.log(error)
-        );
+        return () => subscription.unsubscribe;
     });
 
     const { layout, navTheme, collapse } = basicLayoutState;
@@ -79,70 +72,6 @@ const BasicLayout: React.FC<BasicLayoutProps> = (props) => {
         </section>
     );
 };
-
-// class BasicLayout extends React.PureComponent<BasicLayoutProps, BasicLayoutState> {
-//     public constructor(props: BasicLayoutProps) {
-//         super(props);
-//
-//         this.state = {
-//             ...defaultSettings,
-//             ...JSON.parse(localStorage.getItem(config.SETTINGS_STORAGE_NAME))
-//         };
-//
-//         ['updateCollapse'].forEach((fn) => {
-//             this[fn] = this[fn].bind(this);
-//         });
-//     }
-//
-//     private saveLocalStorage() {
-//         localStorage.setItem(config.SETTINGS_STORAGE_NAME, JSON.stringify(this.state));
-//     }
-//
-//     private updateCollapse() {
-//         this.setState(
-//             {
-//                 collapse: !this.state
-//             },
-//             () => {
-//                 this.saveLocalStorage();
-//             }
-//         );
-//     }
-//
-//     public componentDidMount() {
-//         const { userInfo, saveUserInfo, router } = this.props;
-//
-//         if (utils.isExist(userInfo)) {
-//             return;
-//         }
-//
-//         const subscription = $request.get('/user/info', { metas: { mock: true } }).subscribe((res: Response) => {
-//             if (res.code !== 20000) {
-//                 return router.history.replace(`${rootPath}${loginPath}`);
-//             }
-//
-//             saveUserInfo(res.data.userInfo);
-//         });
-//
-//         subscription.unsubscribe();
-//     }
-//
-//     public render() {
-//         const { children } = this.props;
-//         const { layout, navTheme, collapse } = this.state;
-//
-//         return (
-//             <section className={styles.basicLayoutContainer}>
-//                 {layout === 'sidemenu' && (
-//                     <SideMenuLayout theme={navTheme} collapse={collapse} updateCollapse={this.updateCollapse}>
-//                         {children}
-//                     </SideMenuLayout>
-//                 )}
-//                 {layout === 'topmenu' && <TopMenuLayout>{children}</TopMenuLayout>}
-//             </section>
-//         );
-//     }
-// }
 
 export default connect(BasicLayout, (store) => ({
     userInfo: store.common.userInfo,
